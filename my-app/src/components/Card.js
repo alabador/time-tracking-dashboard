1dcb61cd1072c2cfb9b0ab3e519e471340599067
alabador <austinlabador@gmail.com>
2023-02-05 19:17:10 -0800

    Add more responsive styles

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -11,8 +11,10 @@ function Card(props) {
                     <h2 className='title'>{props.title}</h2>
                     <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/></svg>
                 </div>
-                <h1 className='current-time'>{props.currentTime}hrs</h1>
-                <h3 className="previous-time">Last Week - <span className="previous-time">{props.previousTime}</span></h3>
+                <div className = 'card-time'>
+                    <h1 className='current-time'>{props.currentTime}hrs</h1>
+                    <h3 className="previous-time">Last Week - <span className="previous-time">{props.previousTime}</span></h3>
+                </div>
             </section>
         </div>
     )
